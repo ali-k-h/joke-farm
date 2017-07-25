995b868be632315b669b97cf0360ede8415f2304
Kosar Hosseinkhani <kosarh79@gmail.com>
2017-07-25 11:42:29 -0700

    env

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
-let debug = process.env.NODE_ENV !== "production"
+//let debug = process.env.NODE_ENV !== "prod";
+//console.log(debug + ' ' + process.env.NODE_ENV)
 let webpack = require('webpack')
 let path = require('path')
 
-module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : false,
-  entry: "./js/main.js",
-  module: {
-      loaders: [
-          {
-              test: /\.jsx?$/,
-              exclude: /(node_modules|bower_components)/,
-              loader: 'babel-loader',
-              query: {
-                  presets: ['react', 'es2015', 'stage-0'],
-                  plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-              }
-          }
-      ]
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "bundle.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+module.exports =(env) => {
+    let debug = env !== "prod";
+    console.log(debug + ' ' + env)
+    return {
+        context: path.join(__dirname, "src"),
+        devtool: debug ? "inline-sourcemap" : false,
+        entry: "./js/main.js",
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['react', 'es2015', 'stage-0'],
+                        plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+                    }
+                }
+            ]
+        },
+        output: {
+            path: __dirname + "/src/",
+            filename: "bundle.js"
+        },
+        plugins: debug ? [] : [
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
+        ]
+    }
 };
